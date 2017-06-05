c248a5635612d74c49266f0b084dd9f05e033750
lu11086 <lu11086@qq.com>
2017-06-05 08:34:09 +0800

    尝试更换png为webP格式失败——兼容性问题

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ if (document.documentElement.clientWidth < 767) {
     var myDemosHeight = 4;
 }
 
+/*当浏览器为IE9及以下时自动跳转到github仓库*/
 if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE6.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE7.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE9.0") {
     alert("您的浏览器版本过低，请下载IE9以上版本");
     window.location.href = "https://github.com/lu11086/myResume";
